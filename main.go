1d3d903969d5f7130c1219600767a024bd568092
NickolasHKraus <NickHKraus@gmail.com>
2022-03-18 09:50:21 -0500

    Add '1652. Defuse the Bomb'

--- main.go
+++ main.go
@@ -78,4 +78,61 @@ func insertInterger(integers map[string]bool, runes []rune) map[string]bool {
 	return integers
 }
 
-func main() {}
+// 1652. Defuse the Bomb
+//
+// https://leetcode.com/problems/defuse-the-bomb
+//
+// NOTES
+//   * Use modulo to find the correct index: i = i % n, where n is the length
+//     of the array.
+//
+//   Example:
+//
+//   [0, 1, 2 ... 0, 1, 2]
+//    ^  ^  ^     ^  ^  ^
+//    0  1  2     3  4  5
+//
+//   The 3rd element would be 3 % 3 = arr[0] = 0.
+func Decrypt(code []int, k int) []int {
+
+	// n == code.length and 1 <= n <= 100, therefore allocate an array of size
+	// 100 of type int.
+	decrypted := [100]int{}
+
+	for curr, _ := range code {
+		// If k > 0, replace the ith number with the sum of the next k numbers.
+		// If k < 0, replace the ith number with the sum of the previous k numbers.
+		// If k == 0, replace the ith number with 0.
+		if k > 0 {
+			val := 0
+			for i := 1; i <= k; i++ {
+				val += code[(curr+i)%len(code)]
+			}
+			decrypted[curr] = val
+		} else if k < 0 {
+			val := 0
+			for i := 1; i <= k*-1; i++ {
+				if curr-i < 0 {
+					val += code[len(code)-abs(curr-i)%len(code)]
+				} else {
+					val += code[abs(curr-i)%len(code)]
+				}
+			}
+			decrypted[curr] = val
+		} else {
+			decrypted[curr] = 0
+		}
+	}
+
+	return decrypted[0:len(code)]
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+}
--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestTitleToNumber(t *testing.T) {
 	ret, err := TitleToNumber("A")
@@ -66,6 +69,33 @@ func TestNumDifferentIntegers(t *testing.T) {
 		t.Logf("An error occurred: %s", err)
 		t.Fail()
 	} else if ret != 3 {
+		t.Fail()
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+	// Array values are comparable if values of the array element type are
+	// comparable. Two array values are equal if their corresponding elements are
+	// equal.
+	//
+	// Slice, map, and function values are not comparable. However, as a special
+	// case, a slice, map, or function value may be compared to the predeclared
+	// identifier nil. Comparison of pointer, channel, and interface values to
+	// nil is also allowed and follows from the general rules above.
+	//
+	// https://go.dev/ref/spec#Comparison_operators
+	ret := Decrypt([]int{5, 7, 1, 4}, 3)
+	if !reflect.DeepEqual(ret, []int{12, 10, 16, 13}) {
+		t.Fail()
+	}
+
+	ret = Decrypt([]int{1, 2, 3, 4}, 0)
+	if !reflect.DeepEqual(ret, []int{0, 0, 0, 0}) {
+		t.Fail()
+	}
+
+	ret = Decrypt([]int{2, 4, 9, 3}, -2)
+	if !reflect.DeepEqual(ret, []int{12, 5, 6, 13}) {
 		t.Fail()
 	}
 }
