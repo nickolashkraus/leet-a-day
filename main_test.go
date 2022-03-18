8bd30d96f3ccbaac0fe05d65d88f3b68c821d487
NickolasHKraus <NickHKraus@gmail.com>
2022-03-18 10:31:57 -0500

    Add the following:
      - number_of_different_integers_in_a_string.go
      - number_of_different_integers_in_a_string_test.go

--- main_test.go
+++ main_test.go
@@ -5,40 +5,6 @@ import (
 	"testing"
 )
 
-func TestNumDifferentIntegers(t *testing.T) {
-	ret, err := NumDifferentIntegers("a123bc34d8ef34")
-	if err != nil {
-		t.Logf("An error occurred: %s", err)
-		t.Fail()
-	} else if ret != 3 {
-		t.Fail()
-	}
-
-	ret, err = NumDifferentIntegers("leet1234code234")
-	if err != nil {
-		t.Logf("An error occurred: %s", err)
-		t.Fail()
-	} else if ret != 2 {
-		t.Fail()
-	}
-
-	ret, err = NumDifferentIntegers("a1b01c001")
-	if err != nil {
-		t.Logf("An error occurred: %s", err)
-		t.Fail()
-	} else if ret != 1 {
-		t.Fail()
-	}
-
-	ret, err = NumDifferentIntegers("2393706880236110407059624696967828762752651982730115221690437821508229419410771541532394006597463715513741725852432559057224478815116557380260390432211227579663571046845842281704281749571110076974264971989893607137140456254346955633455446057823738757323149856858154529105301197388177242583658641529908583934918768953462557716z97438020429952944646288084173334701047574188936201324845149110176716130267041674438237608038734431519439828191344238609567530399189316846359766256507371240530620697102864238792350289978450509162697068948604722646739174590530336510475061521094503850598453536706982695212493902968251702853203929616930291257062173c79487281900662343830648295410")
-	if err != nil {
-		t.Logf("An error occurred: %s", err)
-		t.Fail()
-	} else if ret != 3 {
-		t.Fail()
-	}
-}
-
 func TestDecrypt(t *testing.T) {
 	// Array values are comparable if values of the array element type are
 	// comparable. Two array values are equal if their corresponding elements are
---
+++ src/number_of_different_integers_in_a_string.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"strings"
+	"unicode"
+)
+
+// 1805. Number of Different Integers in a String
+//
+// https://leetcode.com/problems/number-of-different-integers-in-a-string
+//
+// NOTES
+//   * Use a hash table for integer lookups.
+func numDifferentIntegers(word string) int {
+
+	integers := make(map[string]bool)
+	runes := []rune{}
+
+	for _, c := range word {
+		if unicode.IsDigit(c) {
+			runes = append(runes, c)
+		} else if len(runes) > 0 {
+			integers = insertInterger(integers, runes)
+			runes = []rune{}
+		}
+	}
+
+	if len(runes) > 0 {
+		integers = insertInterger(integers, runes)
+	}
+
+	return len(integers)
+}
+
+// Insert integer (as string) in map 'integers', if it is not in the map
+func insertInterger(integers map[string]bool, runes []rune) map[string]bool {
+
+	integer := strings.TrimLeft(string(runes), "0")
+
+	// Test that a key is present with a two-value assignment:
+	//
+	//   elem, ok = m[key]
+	//
+	// If key is in m, ok is true. If not, ok is false.
+	//
+	// If key is not in the map, then elem is the zero value for the map's
+	// element type.
+	elem, _ := integers[integer]
+	if !elem {
+		integers[integer] = true
+	}
+
+	return integers
+}
