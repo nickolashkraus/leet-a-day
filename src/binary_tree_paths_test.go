2294dd7a083976e70daaee7fab2cc757ec156f13
NickolasHKraus <NickHKraus@gmail.com>
2022-03-20 11:38:43 -0500

    Add '257. Binary Tree Paths'

---
+++ src/binary_tree_paths_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBinaryTreePaths(t *testing.T) {
+	tree := []string{"1", "2", "3", "null", "5"}
+	// Pointer to the root node of the binary tree.
+	rootNode := binaryTreeFromSlice(tree, 0)
+	res := binaryTreePaths(rootNode)
+	if !reflect.DeepEqual([]string{"1->2->5", "1->3"}, res) {
+		t.Fail()
+	}
+	tree = []string{"1"}
+	// Pointer to the root node of the binary tree.
+	rootNode = binaryTreeFromSlice(tree, 0)
+	res = binaryTreePaths(rootNode)
+	if !reflect.DeepEqual([]string{"1"}, res) {
+		t.Fail()
+	}
+}
+
+func TestBinaryTreeFromSlice(t *testing.T) {
+	tree := []string{"1", "2", "3", "null", "5"}
+	// Pointer to the root node of the binary tree.
+	rootNode := binaryTreeFromSlice(tree, 0)
+	// Check binary tree:
+	//
+	// [1,2,3,null,5]
+	//
+	//     1
+	//   /   \
+	//  2     3
+	//   \
+	//     5
+	if rootNode.Val != 1 {
+		t.Fail()
+	}
+	if rootNode.Left.Val != 2 {
+		t.Fail()
+	}
+	if rootNode.Right.Val != 3 {
+		t.Fail()
+	}
+	if rootNode.Left.Right.Val != 5 {
+		t.Fail()
+	}
+}
